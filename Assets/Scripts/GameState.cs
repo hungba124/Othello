5084bf271abc225954571941ae4898fc06bb9e02
Hungpv <phamvanhung124@gmail.com>
2023-10-05 20:48:04 +0700

    outflanked function

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -28,4 +28,32 @@ public class GameState
 
         CurrentPlayer = Player.Black;
     }
+
+    private bool IsInsideBoar(int r, int c)
+    {
+        return r >= 0 && r < Rows && c >= 0 && c < Cols;
+    }
+
+    private List<Position> OutflankedInDir(Position pos, Player player, int rDelta, int cDelta)
+    {
+        List<Position> outflanked = new List<Position>();
+        int r = pos.Row + rDelta;
+        int c = pos.Col + cDelta;
+
+        while (IsInsideBoar(r, c) && Board[r, c] != Player.None)
+        {
+            if (Board[r, c] === player.Opponent())
+            {
+                outflanked.Add(new Position(r, c));
+                r += rDelta;
+                c += cDelta;
+            }
+            else /*if (Board[r, c] == player)*/
+            {
+                return outflanked;
+            }
+
+            return new List<Position>();
+        }
+    }
 }
