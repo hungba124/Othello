1dea5b65f82e787c636a121cba9824a743de9c12
Hungpv <phamvanhung124@gmail.com>
2023-10-14 16:48:23 +0700

    MakeMove

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 public class GameState
 {
     public const int Rows = 8;
@@ -29,84 +27,102 @@ public class GameState
         CurrentPlayer = Player.Black;
         LegalMoves = FindLegalMove(CurrentPlayer);
     }
-
-    private bool IsInsideBoar(int r, int c)
+    public bool MakeMove(Position pos, out MoveInfo moveInfo)
     {
-        return r >= 0 && r < Rows && c >= 0 && c < Cols;
-    }
-
-    private List<Position> OutflankedInDir(Position pos, Player player, int rDelta, int cDelta)
-    {
-        List<Position> outflanked = new List<Position>();
-        int r = pos.Row + rDelta;
-        int c = pos.Col + cDelta;
-
-        while (IsInsideBoar(r, c) && Board[r, c] != Player.None)
+        if (!LegalMoves.ContainsKey(pos))
         {
-            if (Board[r, c] == player.Opponent())
-            {
-                outflanked.Add(new Position(r, c));
-                r += rDelta;
-                c += cDelta;
-            }
-            else /*if (Board[r, c] == player)*/
-            {
-                return outflanked;
-            }
-        }
-
-        return new List<Position>();
-    }
-
-    private List<Position> Outflanked(Position pos, Player player)
-    {
-        List<Position> outflanked = new List<Position>();
-
-        for (int rDelta = -1; rDelta <= 1; rDelta++)
-        {
-            for (int cDelta = -1; cDelta <= 1; cDelta++)
-            {
-                if (rDelta == 0 && cDelta == 0)
-                {
-                    continue;
-                }
-
-                outflanked.AddRange(OutflankedInDir(pos, player, rDelta, cDelta));
-            }
-        }
-
-        return outflanked;
-    }
-
-    private bool IsMoveLegal(Player player, Position pos, out List<Position> outflanked)
-    {
-        if (Board[pos.Row, pos.Col] != Player.None)
-        {
-            outflanked = null;
+            moveInfo = null;
             return false;
         }
 
-        outflanked = Outflanked(pos, player);
-        return outflanked.Count > 0;
-    }
+        CurrentPlayer movePlayer = CurrentPlayer;
+        List<Position> outflanked = LegalMoves[pos];
 
-    private Dictionary<Position, List<Position>> FindLegalMove(Player player)
-    {
-        Dictionary<Position, List<Position>> legalMoves = new Dictionary<Position, List<Position>>();
+        Board[pos.Row, pos.Column] = movePlayer;
+        // flip discs
+        // update disc counts
+        // pass turn
 
-        for (int r = 0; r < Rows; r++)
+        moveInfo = new MoveInfo { Player = movePlayer, Position = pos, Outflanked = outflanked };
+        return true;
+
+        private bool IsInsideBoar(int r, int c)
         {
-            for (int c  = 0; c < Cols; c++)
-            {
-                Position pos = new Position(r, c);
-
-                if (IsMoveLegal(player, pos, out List<Position> outflanked))
-                {
-                    legalMoves[pos] = outflanked;
-                }
-            }
+            return r >= 0 && r < Rows && c >= 0 && c < Cols;
         }
 
-        return legalMoves;
+        private List<Position> OutflankedInDir(Position pos, Player player, int rDelta, int cDelta)
+        {
+            List<Position> outflanked = new List<Position>();
+            int r = pos.Row + rDelta;
+            int c = pos.Col + cDelta;
+
+            while (IsInsideBoar(r, c) && Board[r, c] != Player.None)
+            {
+                if (Board[r, c] == player.Opponent())
+                {
+                    outflanked.Add(new Position(r, c));
+                    r += rDelta;
+                    c += cDelta;
+                }
+                else /*if (Board[r, c] == player)*/
+                {
+                    return outflanked;
+                }
+            }
+
+            return new List<Position>();
+        }
+
+        private List<Position> Outflanked(Position pos, Player player)
+        {
+            List<Position> outflanked = new List<Position>();
+
+            for (int rDelta = -1; rDelta <= 1; rDelta++)
+            {
+                for (int cDelta = -1; cDelta <= 1; cDelta++)
+                {
+                    if (rDelta == 0 && cDelta == 0)
+                    {
+                        continue;
+                    }
+
+                    outflanked.AddRange(OutflankedInDir(pos, player, rDelta, cDelta));
+                }
+            }
+
+            return outflanked;
+        }
+
+        private bool IsMoveLegal(Player player, Position pos, out List<Position> outflanked)
+        {
+            if (Board[pos.Row, pos.Col] != Player.None)
+            {
+                outflanked = null;
+                return false;
+            }
+
+            outflanked = Outflanked(pos, player);
+            return outflanked.Count > 0;
+        }
+
+        private Dictionary<Position, List<Position>> FindLegalMove(Player player)
+        {
+            Dictionary<Position, List<Position>> legalMoves = new Dictionary<Position, List<Position>>();
+
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Cols; c++)
+                {
+                    Position pos = new Position(r, c);
+
+                    if (IsMoveLegal(player, pos, out List<Position> outflanked))
+                    {
+                        legalMoves[pos] = outflanked;
+                    }
+                }
+            }
+
+            return legalMoves;
+        }
     }
-}
